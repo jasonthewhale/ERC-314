6079fc09b63fcc60c96a50c5bdc6036c3184239a
Junchen You <youjunchen000220@gmail.com>
2024-03-23 19:25:41 +1000

    set fuzz condition, test passed

--- test/ERC314.t.sol
+++ test/ERC314.t.sol
@@ -11,13 +11,14 @@ contract SimpTest is Test {
     address testWallet = 0xF5ED007002Dd362B06284B26e451c3D54210848d;
     address payable mainAddr = payable(0x111fc50541cf398857DF38fA23D586C18E0f96cE);
     uint256 public mainFork;
+    // uint256 tradeAmount = 1 ether;
 
     function setUp() public {
         mainFork = vm.createFork(vm.envString("MAIN_RPC_URL"));
         vm.selectFork(mainFork);
         mainsimp = Simplify_314(mainAddr);
         cusSimp = new Simplify_314();
-        deal(testWallet, 200 ether);
+        deal(testWallet, type(uint256).max);
     }
 
     function testInitialBalance() public view {
@@ -30,14 +31,36 @@ contract SimpTest is Test {
         return mainsimp._maxWallet();
     }
 
-    // Fuzz test here
-    function testTrade(address testWallet, uint256 tradeAmount) public {
+    function selfTransferTest() public {
         vm.startPrank(testWallet);
-
         // Buy
         console.log("Initial ether bal: ", testWallet.balance, "Simp bal: ", mainsimp.balanceOf(testWallet));
-        payable(address(mainsimp)).call{value: tradeAmount}("");
+        payable(address(mainsimp)).call{value: 1 ether}("");
         console.log("Trade 1 ether bal: ", testWallet.balance, "Simp bal: ", mainsimp.balanceOf(testWallet));
+
+        // Roll block
+        vm.roll(block.number + 1);
+
+        // Self transfer check
+        uint256 curBal = mainsimp.balanceOf(testWallet);
+        mainsimp.transfer(testWallet, curBal);
+        console.log("Simp bal: ", mainsimp.balanceOf(testWallet));
+
+        vm.stopPrank();
+    }
+
+    // Fuzz test here
+    function fuzzTestTrade(uint256 tradeAmount) public {
+        // Set test condition
+        tradeAmount = bound(tradeAmount, 0.1 ether, 10000 ether);
+        require(tradeAmount >= 0.1 ether && tradeAmount <= 10000 ether, "Trade amount out of range");
+        
+        vm.startPrank(testWallet);
+        // Buy
+        uint256 preBal = testWallet.balance;
+        // console.log("Initial ether bal: ", preBal, "Simp bal: ", mainsimp.balanceOf(testWallet));
+        payable(address(mainsimp)).call{value: tradeAmount}("");
+        // console.log("Trade 1 ether bal: ", testWallet.balance, "Simp bal: ", mainsimp.balanceOf(testWallet));
 
         // Roll block
         vm.roll(block.number + 1);
@@ -45,7 +68,11 @@ contract SimpTest is Test {
         // Sell
         uint256 curSimpBal = mainsimp.balanceOf(testWallet);
         mainsimp.transfer(address(mainsimp), curSimpBal);
-        console.log("Trade 2 ether bal: ", testWallet.balance, "Simp bal: ", mainsimp.balanceOf(testWallet));
+        uint256 postBal = testWallet.balance;
+        // console.log("Trade 2 ether bal: ", testWallet.balance, "Simp bal: ", mainsimp.balanceOf(testWallet));
+
+        assertTrue(postBal <= preBal);
+
         vm.stopPrank();
     }
 }
